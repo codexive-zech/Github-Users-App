672d1f75505aa10b7993123cfdb56668bb46e017
Codexive_zech <zechariahhounwanou@gmail.com>
2022-05-15 21:08:34 +0100

    implemented the event of click on the button container section
    checked the data index value in the HTML to display the pages index, checked for next btn class increase made sure if the index is bigger than the last array the index value returns to 0, checked for the prev class if it exist in the clicked btn then the index value of the pages should be decreased and if the index value after click is less than 0 the index value be set to the last pages

--- js/app.js
+++ js/app.js
@@ -6,7 +6,7 @@ import paginate from "../utils/paginate.js";
 import displayButtons from "../utils/displayButtons.js";
 
 const title = getElement(".section-title h1");
-const btContainer = getElement(".btn-container");
+const btnContainer = getElement(".btn-container");
 
 // controls dynamically the index of the page
 let index = 0;
@@ -18,7 +18,7 @@ const setupUI = () => {
   //   display each fetched data of a follower into the HTML and passing in array of arrays (pages) and what the current index is
   displayFollower(pages[index]);
   // to display the buttons the button container, array of arrays (pages) is needed and the current index of the page
-  displayButtons(btContainer, pages, index);
+  displayButtons(btnContainer, pages, index);
 };
 // declaring the callback func of invoked in the window object
 const init = async () => {
@@ -28,10 +28,41 @@ const init = async () => {
   title.textContent = "Github Users";
   //
   pages = paginate(followers);
-  console.log(pages);
   // invoking the setup ui so as to display the followers needed each page based on the index
   setupUI();
 };
+
+btnContainer.addEventListener("click", function (e) {
+  // checking to see that as long as it's not the page index value that is been clicked and it's just the side in between of the page then do nothing
+  if (e.target.classList.contains("btn-container")) return;
+  // checking to see if the targeted button clicked has the class of page-btn
+  if (e.target.classList.contains("page-btn")) {
+    // dynamically changing the value of index via the data-index attribute in the button so as to display followers of that page
+    index = parseInt(e.target.dataset.index);
+  }
+  // checking to see if the clicked button has the class of next btn
+  if (e.target.classList.contains("next-btn")) {
+    // the index value should be incremented by 1
+    index++;
+    // checking to see if the new index after increment is bigger than the length of pages (array of arrays) that does display follower
+    if (index > pages.length - 1) {
+      // index show have the value of 0 when it reaches the last index of the data-index attribute
+      index = 0;
+    }
+  }
+  // checking to see if the clicked button has the class of prev btn
+  if (e.target.classList.contains("prev-btn")) {
+    // the index value should be decremented by 1
+    index--;
+    // checking to see if the new index after decrement is less than 0
+    if (index < 0) {
+      // index should be set to the last index value of the pages (array of arrays)
+      index = pages.length - 1;
+    }
+  }
+  // invoking the setup ui so as to display the followers needed each page based on the index
+  setupUI();
+});
 
 // an event that listen to the browser object for a load event
 window.addEventListener("DOMContentLoaded", init);
